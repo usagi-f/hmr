f16bcd03047afa0c40cb51d0deb7dcc1e528cf29
usagi.f@gmail.com <usagi.f@gmail.com>
2018-02-06 22:31:53 +0900

    update: redux base script

---
+++ src/reducers/todos.js
@@ -0,0 +1,22 @@
+import { ADD_TODO } from '../actions/';
+
+const initialState = {
+    todos: []
+}
+
+function todos(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO: // typeを頼りに処理を分岐する
+      return Object.assign({}, state, { // 新しいStateを生成して返す
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      })
+    default: // フォールバック処理
+      return state
+  }
+}
